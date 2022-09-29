96291ecb1f0885f8c6f57ca1b612c82878ea13da
natus <natus@DESKTOP-ECJ060J>
2022-09-29 14:03:32 +0500

    Добавьте файлы проекта.

---
+++ PZ_5/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace PZ_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char simvol = 'h';
+            for (int i = -50; i <= 50; i += 10)
+            {
+                Console.WriteLine(i);
+            }
+            for (int i = 1; i <= 7; i++)
+            {
+                Console.WriteLine(simvol);
+                simvol++;
+            }
+            for (int i = 0; i <= 7; i++)
+            {
+                for (int j = 0; j <= 7; j++)
+                {
+                    Console.WriteLine(i - j);
+                }
+            }
+            for (int i = 0; i < 100; i++)
+            {
+                if (1 % 12 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            for (int i = 3, j = 50; j - i >= 14; i++, j--)
+            {
+                Console.WriteLine(i - j);
+            }
+        }
+    }
+}
